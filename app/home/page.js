6a2333f9e45ab20b8c90a711296753b0f2f2dea7
Fabio Squizzato <fsquizzato@visualmedica.com>
2025-01-03 16:32:21 -0300

    fix: wrapping use searchparam

--- app/home/page.js
+++ app/home/page.js
@@ -1,64 +1,70 @@
-'use client'
+"use client";
 
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { useSearchParams } from 'next/navigation';
+import axios from "axios";
+import { Suspense, useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
 
 const TopTracks = () => {
-    const searchParams = useSearchParams();
-    const [tracks, setTracks] = useState([]);
-    const [totalTracks, setTotalTracks] = useState(false);
-    const access_token = searchParams.get('access_token');
+  const searchParams = useSearchParams();
+  const [tracks, setTracks] = useState([]);
+  const [totalTracks, setTotalTracks] = useState(false);
+  const access_token = searchParams.get("access_token");
 
-    useEffect(() => {
-        const fetchTopTracks = async (offset = 0) => {
-            if (!access_token) return;
+  useEffect(() => {
+    const fetchTopTracks = async (offset = 0) => {
+      if (!access_token) return;
 
-            try {
-                const response = await axios.get('https://api.spotify.com/v1/me/top/tracks', {
-                    headers: {
-                        Authorization: `Bearer ${access_token}`
-                    },
-                    params: {
-                        time_range: 'long_term',
-                        limit: 50,
-                        offset: offset
-                    }
-                });
+      try {
+        const response = await axios.get(
+          "https://api.spotify.com/v1/me/top/tracks",
+          {
+            headers: {
+              Authorization: `Bearer ${access_token}`,
+            },
+            params: {
+              time_range: "long_term",
+              limit: 50,
+              offset: offset,
+            },
+          }
+        );
 
-                // Actualiza el total de pistas si no está configurado
-                if (!totalTracks) {
-                    setTotalTracks(response.data.total);
-                }
+        // Actualiza el total de pistas si no está configurado
+        if (!totalTracks) {
+          setTotalTracks(response.data.total);
+        }
 
-                // Agrega las pistas obtenidas a la lista existente
-                setTracks(prevTracks => [...prevTracks, ...response.data.items]);
+        // Agrega las pistas obtenidas a la lista existente
+        setTracks((prevTracks) => [...prevTracks, ...response.data.items]);
 
-                // Si aún no se han obtenido todas las pistas, realiza otra petición
+        // Si aún no se han obtenido todas las pistas, realiza otra petición
 
-                if (offset + 50 < response.data.total) {
-                    fetchTopTracks(offset + 50);
-                }
-            } catch (error) {
-                console.error('Error fetching top tracks:', error);
-            }
-        };
+        if (offset + 50 < response.data.total) {
+          fetchTopTracks(offset + 50);
+        }
+      } catch (error) {
+        console.error("Error fetching top tracks:", error);
+      }
+    };
 
-        fetchTopTracks();
-    }, [access_token]);
+    fetchTopTracks();
+  }, [access_token]);
 
-    return (
-        <div>
-            <h1>Tus pistas más escuchadas a largo plazo</h1>
-            <ul>
-                {tracks.map((track, index) => (
-                    <li key={track.id}>
-                        {index + 1} {track.name} - {track.artists.map(artist => artist.name).join(', ')}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <Suspense>
+      <div>
+        <h1>Tus pistas más escuchadas a largo plazo</h1>
+        <ul>
+          {tracks.map((track, index) => (
+            <li key={track.id}>
+              {index + 1} {track.name} -{" "}
+              {track.artists.map((artist) => artist.name).join(", ")}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Suspense>
+  );
 };
 
 export default TopTracks;
