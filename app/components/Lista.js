1cce2ae02ebac829714452449e4f2b29a3b38d0a
Fabio Squizzato <fsquizzato@visualmedica.com>
2025-01-06 12:42:29 -0300

    update: secondary text styles

--- app/components/Lista.js
+++ app/components/Lista.js
@@ -1,21 +1,34 @@
-import * as React from 'react';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Avatar from '@mui/material/Avatar';
+import * as React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import { makeStyles } from "@mui/styles";
+const useStyles = makeStyles((theme) => ({
+  secondary: {
+    color: "#686868", // Cambia a tu color deseado
+    fontStyle: "italic",
+  },
+}));
 
-export default function Lista({items}) {
+export default function Lista({ items }) {
+  const classes = useStyles();
   return (
-    <List sx={{ width: '100%', maxWidth: 360 }}>
-      {items.map((item,index)=>(
-        <ListItem key={item.id} sx={{bgcolor:"#1e1e1e", marginBottom:"1em",borderRadius:"12px"}}>
+    <List sx={{ width: "100%", maxWidth: 360 }}>
+      {items.map((item, index) => (
+        <ListItem
+          key={item.id}
+          sx={{ bgcolor: "#1e1e1e", marginBottom: "1em", borderRadius: "12px" }}
+        >
           <ListItemAvatar>
-            <Avatar sx={{bgcolor:"#18ba17"}}>
-              {index+1}
-            </Avatar>
+            <Avatar sx={{ bgcolor: "#18ba17" }}>{index + 1}</Avatar>
           </ListItemAvatar>
-          <ListItemText primary={item.name} secondary={item.artists.map(artist => artist.name).join(', ')} />
+          <ListItemText
+            primary={item.name}
+            secondary={item.artists.map((artist) => artist.name).join(", ")}
+            secondaryTypographyProps={{ className: classes.secondary }}
+          />
         </ListItem>
       ))}
     </List>
