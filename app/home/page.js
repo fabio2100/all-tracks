1935c6fa02da953c25e5297478e5a87321a24114
Fabio Squizzato <fsquizzato@visualmedica.com>
2025-05-08 17:26:34 -0300

    feature: tabs system

--- app/home/page.js
+++ app/home/page.js
@@ -1,10 +1,29 @@
+'use client'
+import { useState } from "react";
 import TopTracksWrapper from "../components/TopTracksWrapper";
-import styles from "../page.module.css"
-export default function home() {
+import styles from "../page.module.css";
+
+export default function Home() {
+  const [activeTab, setActiveTab] = useState("canciones");
+
   return (
     <div className={styles.divHome}>
-      
-      <TopTracksWrapper/>
+      {/* Contenedor de pestañas estilo Bulma */}
+      <div className="tabs is-fullwidth ">
+        <ul>
+          <li className={activeTab === "canciones" ? "is-active" : ""}>
+            <a onClick={() => setActiveTab("canciones")}>Canciones</a>
+          </li>
+          <li className={activeTab === "artistas" ? "is-active" : ""}>
+            <a onClick={() => setActiveTab("artistas")}>Artistas</a>
+          </li>
+        </ul>
+      </div>
+
+      {/* Contenido dinámico basado en la pestaña seleccionada */}
+      <div className="content">
+        {activeTab === "canciones" ? <TopTracksWrapper /> : <p>Artistas</p>}
+      </div>
     </div>
   );
 }
