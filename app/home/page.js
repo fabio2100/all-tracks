539a84e5b8f8c7e6d7d06a9c3d345e8ce5f064ba
fabio2100 <squizzato.fabio@gmail.com>
2025-05-10 18:05:37 -0300

    fix: suspense in artists

--- app/home/page.js
+++ app/home/page.js
@@ -1,21 +1,22 @@
-'use client'
-import { useState } from "react";
+"use client";
+import { Suspense, useState } from "react";
 import TopTracksWrapper from "../components/TopTracksWrapper";
 import styles from "../page.module.css";
 import { useRouter } from "next/navigation";
 import Artistas from "../components/Artistas";
+import { CircularProgress } from "@mui/material";
 
 export default function Home() {
   const [activeTab, setActiveTab] = useState("canciones");
   const router = useRouter();
-  
+
   const handleLogout = () => {
     router.push("/");
   };
 
   return (
     <>
-    <div className={styles.header}>
+      <div className={styles.header}>
         <h2 style={{ marginTop: ".5em", marginBottom: "0.25em" }}>
           Tus pistas escuchadas
         </h2>
@@ -23,26 +24,44 @@ export default function Home() {
           Logout
         </p>
       </div>
-    <div className={styles.divHome}>
-      {/* Contenedor de pestañas estilo Bulma */}
-      <div className="tabs is-fullwidth ">
-        <ul>
-          <li className={activeTab === "canciones" ? "is-active" : ""}>
-            <a onClick={() => setActiveTab("canciones")}>Canciones</a>
-          </li>
-          <li className={activeTab === "artistas" ? "is-active" : ""}>
-            <a onClick={() => setActiveTab("artistas")}>Artistas</a>
-          </li>
-        </ul>
-      </div>
+      <div className={styles.divHome}>
+        {/* Contenedor de pestañas estilo Bulma */}
+        <div className="tabs is-fullwidth ">
+          <ul>
+            <li className={activeTab === "canciones" ? "is-active" : ""}>
+              <a onClick={() => setActiveTab("canciones")}>Canciones</a>
+            </li>
+            <li className={activeTab === "artistas" ? "is-active" : ""}>
+              <a onClick={() => setActiveTab("artistas")}>Artistas</a>
+            </li>
+          </ul>
+        </div>
 
-      {/* Contenido dinámico basado en la pestaña seleccionada */}
-      <div className="content">
-        {console.log(activeTab)}
-        <div style={activeTab === "canciones" ? {display:'block'} : {display:'none '}} ><TopTracksWrapper /></div>
-      <div style={activeTab==='artistas'?{display:'block'}:{display:'none'}}><Artistas /></div>
+        {/* Contenido dinámico basado en la pestaña seleccionada */}
+        <div className="content">
+          {console.log(activeTab)}
+          <div
+            style={
+              activeTab === "canciones"
+                ? { display: "block" }
+                : { display: "none " }
+            }
+          >
+            <TopTracksWrapper />
+          </div>
+          <div
+            style={
+              activeTab === "artistas"
+                ? { display: "block" }
+                : { display: "none" }
+            }
+          >
+            <Suspense fallback={<div><CircularProgress color='success' /></div>}>
+              <Artistas />
+            </Suspense>
+          </div>
+        </div>
       </div>
-    </div>
     </>
   );
 }
